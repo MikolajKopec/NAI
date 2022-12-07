425d0e9e12dcd7a4f367addf0cc577b990d76c32
Mikolaj Kopec <mikolajkopec772@gmail.com>
2022-12-07 22:56:03 +0100

    lab07

--- Lab07/main.cpp
+++ Lab07/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui_c.h>
@@ -6,8 +5,18 @@ using namespace std;
 using namespace cv;
 int main( int argc, char** argv )
 {
-    VideoCapture vc(0);
-    cv::namedWindow("Okienko Prawie Inteligentne", WINDOW_AUTOSIZE);
-    cv::waitKey(0);
+    using namespace cv;
+    VideoCapture vc(1);
+    if (!vc.isOpened() ) return 1;
+    cv::Mat frame;
+
+    cv::namedWindow("Window", WINDOW_AUTOSIZE); // Create a window for display.
+    int i = 0;
+    while (waitKey(10) != 27) {
+        vc >> frame;
+        std::cout << "frame info: " << frame.rows << " " << frame.cols << std::endl;
+        cv::imshow("Frame", frame);
+
+    }
     return 0;
 }
